da7b9ffd6360a511903231e058fe28d8c04b04c8
Krunal Patel <122041677+Harbringer904@users.noreply.github.com>
2026-03-06 00:01:29 +0530

    Adding new bonus instructions
    Adding bonus instructions and editing a few functions

--- SimpleAssembler/Encoder.py
+++ SimpleAssembler/Encoder.py
@@ -42,16 +42,16 @@ def encode_s(op, opcode, f3):#store command
     return (imm_u+rs2+rs1 +f3 + imm_l +opcode)
 
 def encode_b(opers, opc, func3, pc, labels):# branch inst
-    rs1 = getReg(opers[0])
+    rs1 = getReg(opers[0])# def r1
     rs2 = getReg(opers[1])
 
     label_or_imm = opers[2].strip()
-    if label_or_imm in labels:#  implimenting offset in branching
+    if label_or_imm in labels:#  implimenting offset 
         offsett = labels[label_or_imm] - pc
     else:
         offsett = read_imm(label_or_imm)
 
-    imm = imm_to_bin(offsett, 13)# converint into 13 bit bin
+    imm = imm_to_bin(offsett, 13)
 
     b12=imm[0]
     b11=imm[1]
@@ -59,24 +59,22 @@ def encode_b(opers, opc, func3, pc, labels):# branch inst
     b4_1=imm[8:12]
     return b12 + b10_5 + rs2 + rs1 + func3 + b4_1 + b11 + opc
 
-
-def encode_u(opers, opc):# Upper imm inst
+def encode_u(opers, opc):
     rd = getReg(opers[0])
     imm_no = read_imm(opers[1])
-    imm = imm_to_bin(imm_no, 20, unsigned=True)# converting into 20 bit bin
+    imm = imm_to_bin(imm_no, 20, unsigned=True)
     return imm + rd + opc
 
-
-def encode_j(opers, opc, pc, labels):# jump inst
+def encode_j(opers, opc, pc, labels):
     rd = getReg(opers[0])
 
     label_or_imm = opers[1].strip()
     if label_or_imm in labels:
-        offsett = labels[label_or_imm] - pc# offset by label adress
+        offsett = labels[label_or_imm] - pc
     else:
-        offsett = read_imm(label_or_imm)# numeric offset
+        offsett = read_imm(label_or_imm)
 
-    imm = imm_to_bin(offsett, 21)# converting into 21 bit bin
+    imm = imm_to_bin(offsett, 21)
 
     b20    = imm[0]
     b19_12 = imm[1:9]
@@ -84,6 +82,23 @@ def encode_j(opers, opc, pc, labels):# jump inst
     b10_1  = imm[10:20]
     return b20 + b10_1 + b11 + b19_12 + rd + opc
 
+#rgst == 01011
+def encode_rst():
+    # reset command, custom opc 0001011
+    return "00000000000000000000000000001011"
+
+# stop executing code
+def encode_halt():
+    # stops execution, custom opc 0101011
+    return "00000000000000000000000000101011"
+
+# rd and rs1 into bin
+def encode_rvrs(opers):
+    # Format: 0000000 00000 rs1 000 rd 1111011, custom opc 1111011
+    rd  = getReg(opers[0])
+    rs1 = getReg(opers[1])
+
+    return "0000000" + "00000" + rs1 + "000" + rd + "1111011"
 
 def encode(neumonic, operands, pc, labels):
     itype,opcode, f3, f7 = INSTRUCTIONS[neumonic]
@@ -94,9 +109,15 @@ def encode(neumonic, operands, pc, labels):
         return encode_i(neumonic, operands, opcode, f3)
     elif itype == "S":
         return encode_s(operands, opcode,f3)
-    if itype == "B":
+    elif itype == "B":
         return encode_b(operands, opcode, f3, pc, labels)
     elif itype == "U":
         return encode_u(operands, opcode)
     elif itype == "J":
         return encode_j(operands, opcode, pc, labels)
+    elif itype == "RST":
+        return encode_rst()
+    elif itype == "HALT":
+        return encode_halt()
+    elif itype == "RVRS":
+        return encode_rvrs(operands)
